ca8ef4f189895f4af6803216b7da70d02a30de40
mjm5mdm5p4-commits <mjm5mdm5p4@privaterelay.appleid.com>
2026-05-18 20:16:25 +0100

    Create student.js

---
+++ student.js
@@ -0,0 +1,14 @@
+const students = [
+  {
+    matric: "CSC/2025/001",
+    name: "John Doe",
+    dept: "Computer Science",
+    level: "100"
+  },
+  {
+    matric: "CSC/2025/002",
+    name: "Mary Johnson",
+    dept: "Computer Science",
+    level: "200"
+  }
+];
